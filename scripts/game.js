5446eba1665b70b4df4b08bd8d9dc0f0136bb3bc
Karolina <kontakt@karolinakieltyka.pl>
2018-04-28 14:11:13 +0200

    add structure for the canvas

--- scripts/game.js
+++ scripts/game.js
@@ -1,17 +1,30 @@
+class NumberedBox extends createjs.Container {
+    constructor(number= 0) {
+      super();
+      var movieclip = new lib.NumberedBox();
+      movieclip.numberText.text=number;
+      this.addChild(movieclip);
+      //set the random position
+      movieclip.x = Math.random() *200;
+      movieclip.y = Math.random() *200;
+    }
+}
+
 
 class Game{
     constructor() {
       console.log(`Welcome to the game. Version ${this.version()}`);
       this.canvas = document.getElementById("game-canvas");
       this.stage = new createjs.Stage(this.canvas);
+
+      window.debugStage = this.stage;
+
       createjs.Ticker.setFPS(60);
-      createjs.Ticker.on("tick", this.stage);
+      createjs.Ticker.on("tick", this.stage); //for re-drawin the stage
 
-      var circle = new createjs.Shape();
-      circle.graphics.beginFill("blue").drawCircle(0,0,40);
-      circle.x = circle.y = 100;
-      this.stage.addChild(circle);
+      this.stage.addChild(new lib.Background()); //background
 
+      this.stage.addChild(new NumberedBox(88)); //test
     }
     version(){
       return '1.0.0';
