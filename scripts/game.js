57fe2a95533cd0588c9cd17a49cd29523d4d149e
Karolina <kontakt@karolinakieltyka.pl>
2018-04-30 14:11:42 +0200

    click and tap logic

--- scripts/game.js
+++ scripts/game.js
@@ -1,55 +1,66 @@
+// logic for the Count game
+
 class NumberedBox extends createjs.Container {
-    constructor(number= 0) {
-      super();
-      var movieclip = new lib.NumberedBox();
-      movieclip.numberText.text=number;
-      this.addChild(movieclip);
-      this.setBounds(0,0,50,50);
-    
-    }
+  constructor(game, number=0) {
+    super();
+
+    this.game = game;
+
+    var movieclip = new lib.NumberedBox();
+    movieclip.numberText.text = number;
+    this.addChild(movieclip);
+
+    this.setBounds(0,0,50,50);
+
+    // handle click/tap
+    this.on('click', this.handleClick.bind(this));
+  }
+  handleClick() {
+    this.game.handleClick(this);
+  }
 }
 
-
 class Game{
-    constructor() {
-      console.log(`Welcome to the game. Version ${this.version()}`);
-      this.canvas = document.getElementById("game-canvas");
-      this.stage = new createjs.Stage(this.canvas);
-      this.stage.width = this.canvas.width;
-      this.stage.height = this.canvas.height;
+  constructor() {
+    console.log(`Welcome to the game. Version ${this.version()}`);
 
+    this.canvas = document.getElementById("game-canvas");
+    this.stage = new createjs.Stage(this.canvas);
 
-      window.debugStage = this.stage;
+    this.stage.width = this.canvas.width;
+    this.stage.height = this.canvas.height;
 
-      createjs.Ticker.setFPS(60);
-      createjs.Ticker.on("tick", this.stage); //for re-drawin the stage
+    // enable tap on touch device
+    createjs.Touch.enable(this.stage);
 
-      var circle = new createjs.Shape();
-      circle.graphics.beginFill("blue").drawCircle(0,0,40);
-      circle.x = circle.y = 100;
-      this.stage.addChild(circle);
+    createjs.Ticker.setFPS(60);
 
-      this.stage.addChild(new lib.Background());
+    // keep re-drawing the stage.
+    createjs.Ticker.on("tick", this.stage);
 
-     // this.stage.addChild(new NumberedBox(88)); //test
-      this.generateMultiplyBoxes();
+    // background
+    this.stage.addChild(new lib.Background());
 
-    }
-    version(){
-      return '1.0.0';
-    }
-    generateMultiplyBoxes(amount=10) {
-      //loop for count down
-      for (var i=  amount; i>0; i--) {
-        var movieclip = new NumberedBox(i);
-        this.stage.addChild(movieclip);
-          //set the random position
-        movieclip.x = Math.random() * (this.stage.width-movieclip.getBounds().width);
-        movieclip.y = Math.random() * (this.stage.height-movieclip.getBounds().height);
-      }
+    this.generateMultipleBoxes();
+  }
+  version(){
+    return '1.0.0';
+  }
+  generateMultipleBoxes(amount=10) {
+    for (var i=amount; i>0; i--) {
+      var movieclip = new NumberedBox(this, i);
+      this.stage.addChild(movieclip);
+
+      // set the randam position
+      movieclip.x = Math.random() * (this.stage.width - movieclip.getBounds().width);
+      movieclip.y = Math.random() * (this.stage.height - movieclip.getBounds().height);
     }
   }
-  //alert("test")
-  // start the game
-  var game = new Game();
-  
+  handleClick(numberedBox) {
+    this.stage.removeChild(numberedBox);
+  }
+
+}
+//alert("test");
+// start the game
+var game = new Game();
