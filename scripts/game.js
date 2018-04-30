7e1b899be81eef5d668ef86b58c70557a399f9d3
Karolina <kontakt@karolinakieltyka.pl>
2018-04-30 14:46:09 +0200

    mobile/retina view

--- scripts/game.js
+++ scripts/game.js
@@ -32,7 +32,7 @@ class Game{
 
     // enable tap on touch device
     createjs.Touch.enable(this.stage);
-
+    this.retinalize();
     createjs.Ticker.setFPS(60);
 
     // keep re-drawing the stage.
@@ -58,6 +58,18 @@ class Game{
   }
   handleClick(numberedBox) {
     this.stage.removeChild(numberedBox);
+  } retinalize() {
+      this.stage.width = this.canvas.width;
+      this.stage.height = this.canvas.height;
+      let ratio =  window.devicePixelRatio;
+      if(ratio ===undefined) {
+        return;
+      }
+      this.canvas.setAttribute("width", Math.round(this.stage.width * ratio));
+      this.canvas.setAttribute("height", Math.round(this.stage.height * ratio));
+      this.stage.scaleX = this.stage.scaleY = ratio;
+      this.canvas.style.width = this.stage.width + "px";
+      this.canvas.style.height = this.stage.height + "px";
   }
 
 }
