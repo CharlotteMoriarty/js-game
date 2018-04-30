9da7de1086b79d6a21199e261d2b5ab42ac24164
Karolina <kontakt@karolinakieltyka.pl>
2018-04-30 13:39:28 +0200

    Displaying multiply objejct on stage

--- scripts/game.js
+++ scripts/game.js
@@ -4,9 +4,8 @@ class NumberedBox extends createjs.Container {
       var movieclip = new lib.NumberedBox();
       movieclip.numberText.text=number;
       this.addChild(movieclip);
-      //set the random position
-      movieclip.x = Math.random() *200;
-      movieclip.y = Math.random() *200;
+      this.setBounds(0,0,50,50);
+    
     }
 }
 
@@ -16,18 +15,38 @@ class Game{
       console.log(`Welcome to the game. Version ${this.version()}`);
       this.canvas = document.getElementById("game-canvas");
       this.stage = new createjs.Stage(this.canvas);
+      this.stage.width = this.canvas.width;
+      this.stage.height = this.canvas.height;
+
 
       window.debugStage = this.stage;
 
       createjs.Ticker.setFPS(60);
       createjs.Ticker.on("tick", this.stage); //for re-drawin the stage
 
-      this.stage.addChild(new lib.Background()); //background
+      var circle = new createjs.Shape();
+      circle.graphics.beginFill("blue").drawCircle(0,0,40);
+      circle.x = circle.y = 100;
+      this.stage.addChild(circle);
 
-      this.stage.addChild(new NumberedBox(88)); //test
+      this.stage.addChild(new lib.Background());
+
+     // this.stage.addChild(new NumberedBox(88)); //test
+      this.generateMultiplyBoxes();
+
     }
     version(){
       return '1.0.0';
+    }
+    generateMultiplyBoxes(amount=10) {
+      //loop for count down
+      for (var i=  amount; i>0; i--) {
+        var movieclip = new NumberedBox(i);
+        this.stage.addChild(movieclip);
+          //set the random position
+        movieclip.x = Math.random() * (this.stage.width-movieclip.getBounds().width);
+        movieclip.y = Math.random() * (this.stage.height-movieclip.getBounds().height);
+      }
     }
   }
   //alert("test")
