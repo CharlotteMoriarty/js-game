2a635d3136a49ab1cd93d1383518a33ee4ed454c
Karolina <kontakt@karolinakieltyka.pl>
2018-05-07 09:04:18 +0200

    game over scene

--- scripts/game.js
+++ scripts/game.js
@@ -1,13 +1,15 @@
-// logic for the Count game
-
 class NumberedBox extends createjs.Container {
   constructor(game, number=0) {
     super();
 
     this.game = game;
     this.number = number;
+
     var movieclip = new lib.NumberedBox();
     movieclip.numberText.text = number;
+
+    new createjs.ButtonHelper(movieclip, 0, 1, 2, false, new lib.NumberedBox(), 3);
+
     this.addChild(movieclip);
 
     this.setBounds(0,0,50,50);
@@ -20,9 +22,10 @@ class NumberedBox extends createjs.Container {
   }
 }
 
+// This class controls the game data.
 class GameData {
   constructor() {
-    this.amountOfBox = 3;
+    this.amountOfBox = 20;
     this.resetData();
   }
   resetData() {
@@ -35,9 +38,10 @@ class GameData {
     return (number === this.currentNumber);
   }
   isGameWin() {
-    return (this.currentNumber>this.amountOfBox);
+    return (this.currentNumber > this.amountOfBox);
   }
 }
+
 class Game{
   constructor() {
     console.log(`Welcome to the game. Version ${this.version()}`);
@@ -48,12 +52,18 @@ class Game{
     this.stage.width = this.canvas.width;
     this.stage.height = this.canvas.height;
 
+    this.stage.enableMouseOver();
+
     // enable tap on touch device
     createjs.Touch.enable(this.stage);
+
+    // enable retina screen
     this.retinalize();
+
     createjs.Ticker.setFPS(60);
-    //initalizacion
-    this.GameData = new GameData();
+
+    // game related initialization
+    this.gameData = new GameData();
 
     // keep re-drawing the stage.
     createjs.Ticker.on("tick", this.stage);
@@ -61,7 +71,7 @@ class Game{
     // background
     this.stage.addChild(new lib.Background());
 
-    this.generateMultipleBoxes(this.GameData.amountOfBox);
+    this.generateMultipleBoxes(this.gameData.amountOfBox);
   }
   version(){
     return '1.0.0';
@@ -71,36 +81,43 @@ class Game{
       var movieclip = new NumberedBox(this, i);
       this.stage.addChild(movieclip);
 
-      // set the randam position
+      // randam position
       movieclip.x = Math.random() * (this.stage.width - movieclip.getBounds().width);
       movieclip.y = Math.random() * (this.stage.height - movieclip.getBounds().height);
     }
   }
-  handleClick(NumberedBox) {
-    if(this.GameData.isRightNumber(NumberedBox.number)){
-    this.stage.removeChild(NumberedBox);
-    this.GameData.nextNumber();
+  handleClick(numberedBox) {
+    if (this.gameData.isRightNumber(numberedBox.number)) {
+      this.stage.removeChild(numberedBox);
+      this.gameData.nextNumber();
 
-    if(this.GameData.isGameWin()){
-      var gameOverView = new lib.GameOverView();
-      this.stage.addChild(gameOverView);
+      // is game over?
+      if (this.gameData.isGameWin()) {
+        var gameOverView = new lib.GameOverView();
+        this.stage.addChild(gameOverView);
+      }
     }
   }
-  } retinalize() {
-      this.stage.width = this.canvas.width;
-      this.stage.height = this.canvas.height;
-      let ratio =  window.devicePixelRatio;
-      if(ratio ===undefined) {
-        return;
-      }
-      this.canvas.setAttribute("width", Math.round(this.stage.width * ratio));
-      this.canvas.setAttribute("height", Math.round(this.stage.height * ratio));
-      this.stage.scaleX = this.stage.scaleY = ratio;
-      this.canvas.style.width = this.stage.width + "px";
-      this.canvas.style.height = this.stage.height + "px";
-  }
+  retinalize() {
+    this.stage.width = this.canvas.width;
+    this.stage.height = this.canvas.height;
 
+    let ratio = window.devicePixelRatio;
+    if (ratio === undefined) {
+      return;
+    }
+
+    this.canvas.setAttribute('width', Math.round( this.stage.width * ratio ));
+    this.canvas.setAttribute('height', Math.round( this.stage.height * ratio ));
+
+    this.stage.scaleX = this.stage.scaleY = ratio;
+
+    // Set CSS style
+    this.canvas.style.width = this.stage.width + "px";
+    this.canvas.style.height = this.stage.height + "px";
+  }
 }
+
 //alert("test");
 // start the game
 var game = new Game();
